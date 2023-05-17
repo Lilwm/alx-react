3c4bdd6410488909e423c68cf278734fc335be98
Lilwm <lmiiri95@gmain.com>
2023-05-17 22:23:33 +0300

    task 0

---
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,22 @@
+// import logo from './holberton-logo.jpg';
+// import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src='holberton-logo.jpg' className="App-logo" alt="logo" />
+        <h1>School Dashboard</h1>
+      </header>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      <footer className="App-footer">
+        <p>Copyright 2020 - Holberton School</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
