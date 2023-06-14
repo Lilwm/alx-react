1cbb54db1e7ceeb7d330262a8371c0fd8dbb5393
Lilwm <lmiiri95@gmain.com>
2023-06-14 15:39:30 +0300

    used Map instead of toJS

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -5,16 +5,12 @@ const printBestStudents = (object) => {
 
   const getStudent = seq.filter((value) => value.score > 70);
 
-  const student = getStudent.toJS();
-
-  const formatName = (name) => name.charAt(0).toUpperCase() + name.slice(1);
-
-  Object.keys(student).map((key) => {
-    student[key].firstName = formatName(student[key].firstName);
-    student[key].lastName = formatName(student[key].lastName);
-    return student[key];
+  getStudent.map((student) => {
+    student.firstName = student.firstName.charAt(0).toUpperCase() + student.firstName.slice(1);
+    student.lastName = student.lastName.charAt(0).toUpperCase() + student.lastName.slice(1);
   });
 
-  console.log(student);
+  console.log(getStudent);
 };
+
     
